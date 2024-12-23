7a204a50a64306bdcbb8203fc2ce84d06d11fd85
SmitKarwade <smitkarwade31@gmail.com>
2024-12-23 08:45:07 +0530

    Retrofit service to access data

--- app/src/main/java/com/example/spacego/databaseaccess/RetroService.java
+++ app/src/main/java/com/example/spacego/databaseaccess/RetroService.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface RetroService {
     @GET("/missions")
     Call<List<Space_Data>> getMissions();
+
+    @GET("/missions/organizations")
+    Call<List<Space_Data>> getOrgData(@Query("organization") String organization);
 }
--- app/src/main/java/com/example/spacego/databaseaccess/SpaceRepo.java
+++ app/src/main/java/com/example/spacego/databaseaccess/SpaceRepo.java
@@ -3,7 +3,12 @@ package com.example.spacego.databaseaccess;
 import android.content.Context;
 import android.widget.Toast;
 
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,22 +17,32 @@ import retrofit2.Response;
 public class SpaceRepo {
     private RetroService retroService;
     private Context context;
+    private Map<String, MutableLiveData<ArrayList<Space_Data>>> dataMap = new HashMap<>();
 
     public SpaceRepo(Context context) {
         this.retroService = Retrofit_space.getRetrofitInst();
         this.context = context;
     }
 
-    public void getAllMissions() {
-        Call<List<Space_Data>> call = retroService.getMissions();
+    public MutableLiveData<ArrayList<Space_Data>> getOrgWise(String nameOfOrg) {
+        if (!dataMap.containsKey(nameOfOrg)){
+            dataMap.put(nameOfOrg, new MutableLiveData<>());
+        }
+
+        MutableLiveData<ArrayList<Space_Data>> dataArrayList = dataMap.get(nameOfOrg);
+        Call<List<Space_Data>> call = retroService.getOrgData(nameOfOrg);
         call.enqueue(new Callback<List<Space_Data>>() {
+            ArrayList<Space_Data> items = new ArrayList<>();
             @Override
             public void onResponse(Call<List<Space_Data>> call, Response<List<Space_Data>> response) {
                 if (response.isSuccessful() && response.body() != null){
-                    List<Space_Data> items = response.body();
-                    for (Space_Data mission : items){
-                        Toast.makeText(context, mission.getMissionName() , Toast.LENGTH_SHORT).show();
-                    }
+                    items = (ArrayList<Space_Data>) response.body();
+//                    for (Space_Data mission : items){
+//                        Toast.makeText(context, mission.getMissionName() , Toast.LENGTH_SHORT).show();
+//                    }
+                    dataArrayList.postValue(items);
+                }else {
+                    Toast.makeText(context, "Spring Error", Toast.LENGTH_SHORT).show();
                 }
             }
 
@@ -36,5 +51,6 @@ public class SpaceRepo {
                 Toast.makeText(context, "get " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+        return dataArrayList;
     }
 }
