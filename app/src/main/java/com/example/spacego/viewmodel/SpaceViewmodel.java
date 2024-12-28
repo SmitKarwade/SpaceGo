9a82ebd0560f21bbca0e6e2295841f4acd3dbea0
SmitKarwade <smitkarwade31@gmail.com>
2024-12-28 20:34:34 +0530

    Removed unnecessary object

--- app/src/main/java/com/example/spacego/viewmodel/SpaceViewmodel.java
+++ app/src/main/java/com/example/spacego/viewmodel/SpaceViewmodel.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.spacego.databaseaccess.SpaceRepo;
@@ -14,7 +15,6 @@ import java.util.ArrayList;
 
 public class SpaceViewmodel extends AndroidViewModel {
     private SpaceRepo spaceRepo;
-    private LiveData<ArrayList<Space_Data>>  liveData;
 
     public SpaceViewmodel(@NonNull Application application) {
         super(application);
