d698ca8428be5f52bb1f9b3ac3fdb6c09566fb5a
SmitKarwade <smitkarwade31@gmail.com>
2025-01-01 20:38:14 +0530

    Added calls for cart database

--- app/src/main/java/com/example/spacego/databaseaccess/RetroService.java
+++ app/src/main/java/com/example/spacego/databaseaccess/RetroService.java
@@ -3,7 +3,10 @@ package com.example.spacego.databaseaccess;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface RetroService {
@@ -12,4 +15,13 @@ public interface RetroService {
 
     @GET("/missions/organizations")
     Call<List<Space_Data>> getOrgData(@Query("organization") String organization);
+
+    @GET("/cart/list")
+    Call<List<Cart>> getCart();
+
+    @POST("/cart/addMission")
+    Call<Void> addToCart(@Body Cart item);
+
+    @DELETE("/cart/removeMission")
+    Call<String> removeFromCart(@Query("id") Integer id);  // ResponseBody if object expected
 }
