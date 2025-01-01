932e291ddc2da7156cf50d5df6a18a63373ebb49
SmitKarwade <smitkarwade31@gmail.com>
2025-01-01 20:39:59 +0530

    Added logic to get data from cart db

--- app/src/main/java/com/example/spacego/fragments/TicketFragment.java
+++ app/src/main/java/com/example/spacego/fragments/TicketFragment.java
@@ -3,14 +3,30 @@ package com.example.spacego.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.spacego.R;
+import com.example.spacego.databaseaccess.Cart;
+import com.example.spacego.recylerview.CartAdapter;
+import com.example.spacego.viewmodel.SpaceViewmodel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicketFragment extends Fragment {
+    private SpaceViewmodel viewmodel;
+    List<Cart> cartList = new ArrayList<>();
+    private RecyclerView cartRecylerView;
+    private CartAdapter cartAdapter;
 
     public TicketFragment() {
         // Required empty public constructor
@@ -19,7 +35,27 @@ public class TicketFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ticket, container, false);
+        View view = inflater.inflate(R.layout.fragment_ticket, container, false);
+
+        cartRecylerView = view.findViewById(R.id.cartRecylerView);
+        cartAdapter = new CartAdapter(cartList, getActivity());
+        cartRecylerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        cartRecylerView.setHasFixedSize(true);
+        cartRecylerView.setAdapter(cartAdapter);
+
+
+        viewmodel = new ViewModelProvider(requireActivity()).get(SpaceViewmodel.class);
+        viewmodel.getLiveCart().observe(getActivity(), new Observer<List<Cart>>() {
+            @Override
+            public void onChanged(List<Cart> carts) {
+                cartList.clear();
+                cartList.addAll(carts);
+                cartAdapter.notifyDataSetChanged();
+            }
+        });
+
+        return view;
     }
+
+
 }
