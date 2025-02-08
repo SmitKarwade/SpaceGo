9ee441e30f099ae007b58c67bec7a68b642c8c5e
SmitKarwade <smitkarwade31@gmail.com>
2025-02-08 19:15:50 +0530

    Added VerifyResponse.java

---
+++ app/src/main/java/com/example/spacego/payment/VerifyResponse.java
@@ -0,0 +1,14 @@
+package com.example.spacego.payment;
+
+public class VerifyResponse {
+    private boolean success;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
