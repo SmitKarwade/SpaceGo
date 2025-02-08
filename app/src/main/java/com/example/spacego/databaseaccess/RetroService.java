6f9adc86df1377f41f2653b7a3ae13df92d82823
SmitKarwade <smitkarwade31@gmail.com>
2025-02-08 19:16:54 +0530

    Added call method to process payment

--- app/src/main/java/com/example/spacego/databaseaccess/RetroService.java
+++ app/src/main/java/com/example/spacego/databaseaccess/RetroService.java
@@ -1,10 +1,15 @@
 package com.example.spacego.databaseaccess;
 
+import com.example.spacego.payment.OrderResponse;
+import com.example.spacego.payment.VerifyResponse;
+
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -30,4 +35,16 @@ public interface RetroService {
 
     @POST("/form/add")
     Call<Void> addUserDetails(@Body UserDetails userDetails);
+
+    @FormUrlEncoded
+    @POST("/api/payment/create-order")
+    Call<OrderResponse> createOrder(@Field("amount") double amount);
+
+    @FormUrlEncoded
+    @POST("/api/payment/verify-payment")
+    Call<VerifyResponse> verifyPayment(
+            @Field("paymentId") String paymentId,
+            @Field("orderId") String orderId,
+            @Field("signature") String signature
+    );
 }
